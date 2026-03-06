46e2fda36e5a0c930b0f934f243bea361478606b
kaannerenn <yavuzk20@itu.edu.tr>
2026-03-07 00:14:10 +0300

    try-except komutlarını daha okunur hale getirmek için fonksyion eklendi.

--- scraper.py
+++ scraper.py
@@ -12,39 +12,41 @@ driver.get("https://www.trendyol.com")
 
 WebDriverWait(driver,TIMEOUT).until(expected_conditions.visibility_of_element_located((By.XPATH,"/html/body/div[1]/div/div/div[2]")))
 driver.find_element(By.XPATH,"/html/body/div[1]/div/div/div[2]").click()
+
 WebDriverWait(driver,4).until(expected_conditions.visibility_of_element_located((By.XPATH,"/html/body/header/div[2]/div/div[2]/div/div[2]/div[1]/button[2]")))
 search_bar_trendyol = driver.find_element(By.XPATH,"/html/body/header/div[2]/div/div[2]/div/div[2]/div[1]/button[2]")
 search_bar_trendyol.click()
+
 WebDriverWait(driver,TIMEOUT).until(expected_conditions.visibility_of_element_located((By.NAME,"search-input")))
 search_bar_trendyol_2 = driver.find_element(By.NAME,"search-input")
 search_bar_trendyol_2.send_keys("Acer Bilgisayar")
 search_bar_trendyol_2.send_keys(Keys.ENTER)
+
 WebDriverWait(driver,TIMEOUT).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME,"search-result-content")))
 
-'''product_names = driver.find_elements(By.CLASS_NAME,"title")
-del product_names[0]
-print(len(product_names))
-for i in product_names:
-    print(i.text)
-product_prices = driver.find_elements(By.CLASS_NAME,"price-section")
-print(len(product_prices))
-for i in product_prices:
-    print(i.text)
-'''
+def get_price(card):
+    possible_classes = ["price-section","sale-price","price-value"]
+
+    for class_name in possible_classes:
+        try:
+            product_price = card.find_element(By.CLASS_NAME,class_name).text
+            return product_price
+        except:
+            continue
+    return "Fiyat bulunamadı."
+
 product_cards = driver.find_elements(By.CLASS_NAME,"product-card")
+
 print(len(product_cards))
 for card in product_cards:
     try:
         product_name = card.find_element(By.CLASS_NAME,"title")
-        try:
-            product_price = card.find_element(By.CLASS_NAME,"price-section")
-        except:
-            try:
-                product_price = card.find_element(By.CLASS_NAME,"sale-price")
-            except:
-                product_price = card.find_element(By.CLASS_NAME,"price-value")
-        print(f"Ürün: {product_name.text} | Fiyat: {product_price.text}")
+        product_price = get_price(card)
+        print(f"Ürün: {product_name.text} | Fiyat: {product_price}")
     except:
         continue
-input("Enter Bas")
+
+
+
+input("Enter Bas (Çıkış için)")
 driver.quit()
